27131da2e6d2f794ec91092c97caaf87484df387
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-07-27 01:17:01 -0500

    manejo de cadenas

---
+++ src/Practicas/Manejo_de_cadenas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicas;
+
+/**
+ *
+ * @author DESTROYER
+ */
+public class Manejo_de_cadenas {
+    public static void main (String [] args){
+        String nombre = "Jesus";
+        String apellido = "Morales";
+        System.out.println("Concatenacion: "+nombre+apellido);
+        System.out.println("Nueva linea \n: "+nombre+" "+apellido);
+        System.out.println("Tabulador: \t: " +nombre+" "+apellido);
+        System.out.println("Retroceso \b: "+nombre+" "+apellido);
+        System.out.println("Retorno de carro \r: "+nombre+" "+apellido);
+        System.out.println("Comilla simple \': "+nombre+" "+apellido);
+        System.out.println("Comilla doble \": "+nombre+" "+apellido);
+    }
+}
