1dcd5d529b7cb2afc918a1d0a30b30f610ed0d97
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-07-27 02:34:07 -0500

    manejo de if-else

---
+++ src/Practicas/manejo_if_else.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicas;
+
+/**
+ *
+ * @author DESTROYER
+ */
+public class manejo_if_else {
+    public static void main (String [] args){
+        int x = 10;
+        if(x  == 10){
+            System.out.println("10 es igual a 10");
+        }else if(x == 20){
+           System.out.println("10 es menor a 20");
+        }else if(x == 30){
+            System.out.println("10 es menor a 30");
+        }else {
+            System.out.println("10 no es igual  a 10, ni a 20, ni a 30");
+        }
+    }
+}
