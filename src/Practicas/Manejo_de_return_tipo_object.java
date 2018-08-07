140340593019d5ce9b1992c9e9637ff879f2fb69
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-08-06 23:27:21 -0500

    Manejo de return tipo object

---
+++ src/Practicas/Manejo_de_return_tipo_object.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicas;
+
+/**
+ *
+ * @author DESTROYER
+ */
+public class Manejo_de_return_tipo_object {
+    public static void main(String [] args){
+        Suma s = crearObjetoSuma();
+        int resultado = s.a + s.b;
+        System.out.println("Resultado: "+resultado);
+    }
+    public static Suma crearObjetoSuma(){
+        Suma s= new Suma(3,4);
+        return s;
+    }
+}
+class Suma{
+    int a;
+    int b;
+    Suma(int a, int b){
+this.a = a;
+this.b = b;
+}
+}
+
