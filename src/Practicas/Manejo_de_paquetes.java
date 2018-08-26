dcd2548b409949710b9d19749681481c496c84a2
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-08-25 23:19:33 -0500

    Manejo de paquetes

---
+++ src/Practicas/Manejo_de_paquetes.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicas;//Definicion del paquete
+
+/**
+ *
+ * @author DESTROYER
+ */
+public class Manejo_de_paquetes {
+  public static void imprimir(String s){
+      System.out.println("Imprimiento mensaje: "+s);
+  }  
+}
---
+++ src/Practicas/Manejo_de_paquetes_principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicas;
+import Practicas.Manejo_de_paquetes;
+/**
+ *
+ * @author DESTROYER
+ */
+public class Manejo_de_paquetes_principal {
+    public static void main(String [] args){
+        Manejo_de_paquetes.imprimir("Juan");
+    }
+}
